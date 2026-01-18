86485b69307cc8cea37e79f2771cd535fd18c30d
Chandra Sekhar Das <sekhar.mld@gmail.com>
2026-01-18 22:42:22 +0530

    Bug fixed - db port number

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -6,7 +6,7 @@ import * as schema from './schema';
 // Create a connection pool for better performance
 const poolConnection = mysql.createPool({
 	host: env.DB_HOST || 'rkmvvmmalda.org',
-	port: Number(env.DB_PORT) | 3306,
+	port: Number(env.DB_PORT) || 3306,
 	user: env.DB_USER,
 	password: env.DB_PASS,
 	database: env.DB_NAME,
